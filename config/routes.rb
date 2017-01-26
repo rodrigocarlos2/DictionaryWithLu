7a7cc4fdd3aee64440d3ecba7c89ab86c28291b1
Rodrigo Carlos <rodrigo19962010@live.com>
2017-01-26 12:14:36 -0300

    Add Word with Rodrigo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  get 'home/index', to: 'home#index'
+
+  root to: 'home#index'
+
   resources :words
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
